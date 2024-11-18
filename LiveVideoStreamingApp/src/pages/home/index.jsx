0a4d264d63a9cb787c68d8f069fb61185aeac370
Bharath Pothula <Bharathkkl907@gmail.com>
2024-11-18 15:33:54 +0530

    integrated ZegoCLoud VideoStream

--- LiveVideoStreamingApp/src/pages/home/index.jsx
+++ LiveVideoStreamingApp/src/pages/home/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {useNavigate} from 'react-router-dom'
 
 const index = () => {
+  const [roomCode, setRoomCode] = useState("")
+  const navigate=useNavigate() ;
+  const handleFormsubmit=(ev)=>{
+    ev.preventDefault();
+   navigate(`/room/${roomCode}`);
+  }
   return (
-    <div>
-      
+    <div className="Home-Page">
+      <form onSubmit={handleFormsubmit} className="form">
+        <div className='Room'>
+            <label>Enter Room Card:</label>
+            <input value={roomCode} onChange={(e)=> setRoomCode(e.target.value)} type="text" required placeholder="Enter Room Code" />
+            <button type="submit" className='but'>Enter Room</button>
+        </div>
+      </form>
     </div>
   )
 }
